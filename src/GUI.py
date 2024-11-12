191d0a9e8685bed8654d68238e0049dfa23439e6
thaonee0 <phuongthaopham4814@gmail.com>
2024-11-12 15:00:04 +0700

    comment

--- src/GUI.py
+++ src/GUI.py
@@ -7,7 +7,7 @@ import sys
 import subprocess
 
 # Thêm đường dẫn gốc của project vào PYTHONPATH
-current_dir = os.path.dirname(os.path.abspath(__file__))
+#current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)
 if project_root not in sys.path:
     sys.path.append(project_root)
